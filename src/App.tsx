f54753ec96eb193007dff63ac3f9ec902a98ba39
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2024-09-21 00:45:56 +0545

    refactor: restructure App component layout

--- src/App.tsx
+++ src/App.tsx
@@ -10,20 +10,20 @@ function LoadingFallback() {
 
 function Studio({ children }: PropsWithChildren) {
   return (
-    <Canvas className={'h-full w-full'} camera={{ fov: 35, zoom: 1.3, near: 1, far: 1000 }}>
-      <Suspense fallback={<LoadingFallback />}>{children}</Suspense>
-    </Canvas>
+    <div className={'w-screen h-screen bg-black'}>
+      <Canvas className={'h-full w-full'} camera={{ fov: 35, zoom: 1.3, near: 1, far: 1000 }}>
+        <Suspense fallback={<LoadingFallback />}>{children}</Suspense>
+      </Canvas>
+    </div>
   );
 }
 
 function App() {
   return (
-    <div className={'w-screen h-screen bg-black'}>
-      <Studio>
-        <DepthImagePlane />
-        <OrbitControls />
-      </Studio>
-    </div>
+    <Studio>
+      <DepthImagePlane />
+      <OrbitControls />
+    </Studio>
   );
 }
 
