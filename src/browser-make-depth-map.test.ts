56ef2b653b5719bbb384fe836d61d00f997130c7
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-05-26 14:25:57 +0545

    chore: ignore cache files

--- src/browser-make-depth-map.test.ts
+++ src/browser-make-depth-map.test.ts
@@ -1,29 +1,25 @@
-import { RawImage } from "@xenova/transformers";
+import { RawImage } from '@xenova/transformers';
 
-import { predictDepthFromImage } from "./depth-estimation-predictor.ts";
+import { predictDepthFromImage } from './depth-estimation-predictor.ts';
 
-describe("depth map", async () => {
-  it(
-    "should make depth map from url blob",
-    async () => {
-      // const depth_estimator = await fetchDepthEstimationPipeline();
-      let imageFileUrl: string;
-      imageFileUrl =
-        "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png";
-      imageFileUrl =
-        "https://images.unsplash.com/photo-1747901718105-bf9beb57ba3a?q=80&w=4287&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
-      const prediction = await predictDepthFromImage(imageFileUrl);
-      if (Array.isArray(prediction)) {
-        throw new Error("not supported");
-      }
-      const depth = prediction.depth;
-      const color = await RawImage.read(imageFileUrl);
-      await depth.save("public/test_depth.png");
-      await color.save("public/test_color.png");
+describe('depth map', async () => {
+  it('should make depth map from url blob', async () => {
+    // const depth_estimator = await fetchDepthEstimationPipeline();
+    let imageFileUrl: string;
+    imageFileUrl =
+      'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png';
+    imageFileUrl =
+      'https://images.unsplash.com/photo-1747901718105-bf9beb57ba3a?q=80&w=4287&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+    const prediction = await predictDepthFromImage(imageFileUrl);
+    if (Array.isArray(prediction)) {
+      throw new Error('not supported');
+    }
+    const depth = prediction.depth;
+    const color = await RawImage.read(imageFileUrl);
+    await depth.save('public/test_depth.png');
+    await color.save('public/test_color.png');
 
-      console.log(prediction);
-      expect(1).toBe(1);
-    },
-    { timeout: 300000 },
-  );
+    console.log(prediction);
+    expect(1).toBe(1);
+  }, 300000);
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     globals: true,
-    name: "happy-dom",
+    name: 'happy-dom',
     // root: "./shared_tests",
-    environment: "node",
-    // environment: 'happy-dom',
+    // environment: "node",
+    environment: 'happy-dom',
     // setupFiles: ['./setup.happy-dom.ts'],:
   },
 });
