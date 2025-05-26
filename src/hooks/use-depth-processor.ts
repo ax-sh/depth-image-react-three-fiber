d29f476ebe1d8b8f19f0c677779d03992e9e4087
Axmin Shrestha <56112475+ax-sh@users.noreply.github.com>
2025-05-26 18:15:27 +0545

    style: format import statements and component JSX

--- src/hooks/use-depth-processor.ts
+++ src/hooks/use-depth-processor.ts
@@ -41,3 +41,29 @@ export function useDepthProcessor(files: File[]) {
   }, [files]);
   return { state, eventRef };
 }
+
+function useDepthProcessingWorker() {
+  // Create a reference to the worker object.
+  const worker = useRef<Worker | null>(null);
+
+  // We use the `useEffect` hook to set up the worker as soon as the `App` component is mounted.
+  useLayoutEffect(() => {
+    // Create the worker if it does not yet exist.
+    worker.current ??= new Worker(new URL("./worker.js", import.meta.url), {
+      type: "module",
+    });
+
+    // Create a callback function for messages from the worker thread.
+    const onMessageReceived = (e) => {
+      // TODO: Will fill in later
+    };
+
+    // Attach the callback function as an event listener.
+    worker.current.addEventListener("message", onMessageReceived);
+
+    // Define a cleanup function for when the component is unmounted.
+    return () =>
+      worker.current?.removeEventListener("message", onMessageReceived);
+  });
+  // console.log(worker.current., 3333);
+}
